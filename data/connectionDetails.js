27e1b7c541b5860d4deff34d19c541838d50f131
khanm2758 <khanm2758@gmail.com>
2025-07-29 16:17:02 +0600

    userid and password included

--- data/connectionDetails.js
+++ data/connectionDetails.js
@@ -1,6 +1,6 @@
 const mongoConn = {
-    userid: '',
-    password: ''
-}
+  userid: encodeURIComponent("mohammadmohiuddin2758"),
+  password: encodeURIComponent("iBP#S4Ukct.pzgM"),
+};
 
-module.exports = {mongoConn};
+module.exports = { mongoConn };
